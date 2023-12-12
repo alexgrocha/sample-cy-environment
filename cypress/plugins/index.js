51a34e88c713883f7a12373bacef7ccf2acef18b
Alex Rocha <70808573+alexgrocha@users.noreply.github.com>
2023-12-11 21:03:24 -0300

    cypress
    add automação cypress

---
+++ cypress/plugins/index.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+// require('dotenv').config()
+// require('dotenv').config({path: `${process.cwd()}/cypress/support/config/local.env`})
+const dotenvPlugin = require('cypress-dotenv')
+
+module.exports = (on, config) => {
+    // config.env.HOST = process.env.CYPRESS_HOST
+    // config.env.MY_ENV = process.env.CYPRESS_MY_ENV
+    config = dotenvPlugin(config)
+    return config
+}
